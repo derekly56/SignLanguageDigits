7bddbb28b27be8bf178dd0308c914044fd0ed6f9
Derek <derek.ly28@gmail.com>
2020-09-27 13:53:14 -0600

    Fixed bias shape

--- ml_model/digits_neural_network.py
+++ ml_model/digits_neural_network.py
@@ -3,8 +3,6 @@ import pandas as pd
 import tensorflow as tf
 import math
 
-from sklearn.model_selection import train_test_split
-
 class DigitNeuralNetwork:
     def __init__(self, epochs:int = 100, batch_size:int = 32):
         '''Initialize Digit NN'''
@@ -40,12 +38,12 @@ class DigitNeuralNetwork:
             print("Invalid Activation function, returning original tensor")
             return y_matrix
 
-    def _create_weights_and_bias(self, weights_shape: list, bias_shape: int) -> tf.Tensor:
+    def _create_weights_and_bias(self, weights_shape: list, bias_shape: list) -> tf.Tensor:
         '''Creates a randomized weight and bias tensor to be used for NN calculations
         
         Args:
             weights_shape (list): Weight tensor shape
-            bias_shape (int): Bias tensor shape
+            bias_shape (list): Bias tensor shape
         
         Returns:
             W_n (tf.Tensor): Weight tensor
@@ -71,19 +69,19 @@ class DigitNeuralNetwork:
         is_training = tf.compat.v1.placeholder(dtype=tf.bool, name='is_training')
 
         # Conv Hidden Layer 1 - Weights/Bias -> Layer Calculation
-        W1, b1 = self._create_weights_and_bias([3, 3, 3, 32], 32)
+        W1, b1 = self._create_weights_and_bias([3, 3, 3, 32], [32])
         b1_conv = tf.add(tf.nn.conv2d(input=x, filter=W1, strides=[1,1,1,1], padding='SAME'), b1)
         b1_conv_batch_norm = tf.compat.v1.layers.batch_normalization(b1_conv, training=is_training)
         b1_conv_activation = self._activation('ReLu', b1_conv_batch_norm)
 
         # Conv Hidden Layer 2 - Weights/Bias -> Layer Calculation
-        W2, b2 = self._create_weights_and_bias([3, 3, 32, 64], 64)
+        W2, b2 = self._create_weights_and_bias([3, 3, 32, 64], [64])
         b2_conv = tf.add(tf.nn.conv2d(input=b1_conv_activation, filter=W2, strides=[1,1,1,1], padding='SAME'), b2)
         b2_conv_batch_norm = tf.compat.v1.layers.batch_normalization(b2_conv, training=is_training)
         b2_conv_activation = self._activation('ReLu', b2_conv_batch_norm)
 
         # Conv Hidden Layer 3 - Weights/Bias -> Layer Calculation
-        W3, b3 = self._create_weights_and_bias([3, 3, 64, 128], 128)
+        W3, b3 = self._create_weights_and_bias([3, 3, 64, 128], [128])
         b3_conv = tf.add(tf.nn.conv2d(input=b2_conv_activation, filter=W3, strides=[1,1,1,1], padding='SAME'), b3)
         b3_conv_batch_norm = tf.compat.v1.layers.batch_normalization(b3_conv, training=is_training)
         b3_conv_activation = self._activation('ReLu', b3_conv_batch_norm)
@@ -94,19 +92,19 @@ class DigitNeuralNetwork:
         flatten_conv = tf.reshape(b3_conv_activation, [-1, num_features])
 
         # FC Hidden Layer 4 - Weights/Bias -> Layer Calculation
-        W4, b4 = self._create_weights_and_bias([flatten_conv.get_shape()[1:4].num_elements(), 256], 256)
+        W4, b4 = self._create_weights_and_bias([flatten_conv.get_shape()[1:4].num_elements(), 256], [256])
         fc_layer4 = tf.add(tf.matmul(flatten_conv, W4), b4)
         fc_layer4_batch_norm = tf.compat.v1.layers.batch_normalization(fc_layer4, training=is_training)
         fc_layer4_activation = self._activation('ReLu', fc_layer4_batch_norm)
 
         # FC Hidden Layer 5 - Weights/Bias -> Layer Calculation
-        W5, b5 = self._create_weights_and_bias([256, 64], 64)
+        W5, b5 = self._create_weights_and_bias([256, 64], [64])
         fc_layer5 = tf.add(tf.matmul(fc_layer4_activation, W5), b5)
         fc_layer5_batch_norm = tf.compat.v1.layers.batch_normalization(fc_layer5, training=is_training)
         fc_layer5_activation = self._activation('ReLu', fc_layer5_batch_norm)
 
         # FC Hidden Layer 6 - Weights/Bias -> Layer Calculation
-        W6, b6 = self._create_weights_and_bias([64, 10], 10)
+        W6, b6 = self._create_weights_and_bias([64, 10], [10])
         fc_layer6 = tf.add(tf.matmul(fc_layer5_activation, W6), b6)
         fc_layer6_batch_norm = tf.compat.v1.layers.batch_normalization(fc_layer6, training=is_training)
         y_preds = self._activation('Softmax', fc_layer6_batch_norm)
@@ -126,7 +124,6 @@ class DigitNeuralNetwork:
             init = tf.compat.v1.global_variables_initializer()
             sess.run(init)
 
-            # 2000 / 50 = 40
             total_batch_size = math.ceil(X_train.shape[0] / self.batch_size)
 
             for epoch in range(1, self.epochs):
